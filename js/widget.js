90a07d88d063aeca1eeee46e2366815d9f3379dd
Latnove <88102671+Latnove@users.noreply.github.com>
2022-11-09 14:59:27 +0300

    Add files via upload

--- js/widget.js
+++ js/widget.js
@@ -941,10 +941,14 @@ widgetEl.addEventListener("wheel", (e) => {
 });
 
 widgetEl.addEventListener("touchmove", function (e) {
-  document.body.style.overflow = "fixed";
+  document.body.style.overflow = "hidden";
+  if (window.innerWidth <= 500)
+    document.body.style.position = "fixed";
   userWidgetUsingChecker(e);
 });
 
 widgetEl.addEventListener("touchend", function (e) {
   document.body.style.overflow = "auto";
+  if (window.innerWidth <= 500)
+    document.body.style.position = "initial";
 });
